9d6a331b481a0d3f9ccc9f17a7dff84b47907150
Harshwardhan <harshwardhanshetty6@gmail.com>
2023-11-06 21:45:15 +0530

    insert function

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"fmt"
 	"log"
+	model "mongotut/models"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -30,4 +33,27 @@ func init() {
 	collection = client.Database(dbName).Collection(colName)
 
 	fmt.Println("Collection instance is ready")
+}
+
+//Mongodb helpers - file
+
+//insert 1 record
+func insertOneMovie(movie model.Netflix)  {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+	if err!=nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted 1 movie in db with id: ", inserted.InsertedID)
+}
+
+func updatedOneMovie(movieId string)  {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id":id}
+	update:= bson.M{"set":bson.M{"watched":true}}
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err!=nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("modified count: ",result.ModifiedCount)
 }
